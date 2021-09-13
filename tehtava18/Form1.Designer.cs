7fdb92fb052000b0a3066a1fef9f866413a5cfa8
Veikko Pihlajamaa <veikko.pihlajamaa@edu.keuda.fi>
2021-09-13 14:16:55 +0300

    Add project files.

---
+++ tehtava18/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace tehtava18
+{
+    partial class henkilot_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.osoite_label = new System.Windows.Forms.Label();
+            this.postinuero_label = new System.Windows.Forms.Label();
+            this.postitoimi_label = new System.Windows.Forms.Label();
+            this.puhelin_label = new System.Windows.Forms.Label();
+            this.puheli_henkilo_label = new System.Windows.Forms.Label();
+            this.sahkopost_label = new System.Windows.Forms.Label();
+            this.osasto_label = new System.Windows.Forms.Label();
+            this.titteli_label = new System.Windows.Forms.Label();
+            this.oppilaitos_comboBox = new System.Windows.Forms.ComboBox();
+            this.vastuuhenkilo_comboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oppilaitoksen avain henkilöt";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valitse oppilaitos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(324, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(222, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Valitse vastuuhenkilö:";
+            // 
+            // osoite_label
+            // 
+            this.osoite_label.AutoSize = true;
+            this.osoite_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.osoite_label.Location = new System.Drawing.Point(59, 146);
+            this.osoite_label.Name = "osoite_label";
+            this.osoite_label.Size = new System.Drawing.Size(74, 25);
+            this.osoite_label.TabIndex = 5;
+            this.osoite_label.Text = "Osoite";
+            // 
+            // postinuero_label
+            // 
+            this.postinuero_label.AutoSize = true;
+            this.postinuero_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postinuero_label.Location = new System.Drawing.Point(59, 187);
+            this.postinuero_label.Name = "postinuero_label";
+            this.postinuero_label.Size = new System.Drawing.Size(118, 25);
+            this.postinuero_label.TabIndex = 6;
+            this.postinuero_label.Text = "Postiosoite";
+            // 
+            // postitoimi_label
+            // 
+            this.postitoimi_label.AutoSize = true;
+            this.postitoimi_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postitoimi_label.Location = new System.Drawing.Point(59, 225);
+            this.postitoimi_label.Name = "postitoimi_label";
+            this.postitoimi_label.Size = new System.Drawing.Size(146, 25);
+            this.postitoimi_label.TabIndex = 7;
+            this.postitoimi_label.Text = "Postitoimialue";
+            // 
+            // puhelin_label
+            // 
+            this.puhelin_label.AutoSize = true;
+            this.puhelin_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puhelin_label.Location = new System.Drawing.Point(59, 264);
+            this.puhelin_label.Name = "puhelin_label";
+            this.puhelin_label.Size = new System.Drawing.Size(84, 25);
+            this.puhelin_label.TabIndex = 8;
+            this.puhelin_label.Text = "Puhelin";
+            // 
+            // puheli_henkilo_label
+            // 
+            this.puheli_henkilo_label.AutoSize = true;
+            this.puheli_henkilo_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puheli_henkilo_label.Location = new System.Drawing.Point(325, 264);
+            this.puheli_henkilo_label.Name = "puheli_henkilo_label";
+            this.puheli_henkilo_label.Size = new System.Drawing.Size(84, 25);
+            this.puheli_henkilo_label.TabIndex = 12;
+            this.puheli_henkilo_label.Text = "Puhelin";
+            // 
+            // sahkopost_label
+            // 
+            this.sahkopost_label.AutoSize = true;
+            this.sahkopost_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sahkopost_label.Location = new System.Drawing.Point(325, 225);
+            this.sahkopost_label.Name = "sahkopost_label";
+            this.sahkopost_label.Size = new System.Drawing.Size(119, 25);
+            this.sahkopost_label.TabIndex = 11;
+            this.sahkopost_label.Text = "Sähköposti";
+            // 
+            // osasto_label
+            // 
+            this.osasto_label.AutoSize = true;
+            this.osasto_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.osasto_label.Location = new System.Drawing.Point(324, 187);
+            this.osasto_label.Name = "osasto_label";
+            this.osasto_label.Size = new System.Drawing.Size(80, 25);
+            this.osasto_label.TabIndex = 10;
+            this.osasto_label.Text = "Osasto";
+            // 
+            // titteli_label
+            // 
+            this.titteli_label.AutoSize = true;
+            this.titteli_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titteli_label.Location = new System.Drawing.Point(324, 146);
+            this.titteli_label.Name = "titteli_label";
+            this.titteli_label.Size = new System.Drawing.Size(64, 25);
+            this.titteli_label.TabIndex = 9;
+            this.titteli_label.Text = "Titteli";
+            // 
+            // oppilaitos_comboBox
+            // 
+            this.oppilaitos_comboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.oppilaitos_comboBox.FormattingEnabled = true;
+            this.oppilaitos_comboBox.Location = new System.Drawing.Point(64, 101);
+            this.oppilaitos_comboBox.Name = "oppilaitos_comboBox";
+            this.oppilaitos_comboBox.Size = new System.Drawing.Size(176, 33);
+            this.oppilaitos_comboBox.TabIndex = 13;
+            this.oppilaitos_comboBox.SelectedIndexChanged += new System.EventHandler(this.oppilaitos_comboBox_SelectedIndexChanged);
+            // 
+            // vastuuhenkilo_comboBox
+            // 
+            this.vastuuhenkilo_comboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vastuuhenkilo_comboBox.FormattingEnabled = true;
+            this.vastuuhenkilo_comboBox.Location = new System.Drawing.Point(330, 101);
+            this.vastuuhenkilo_comboBox.Name = "vastuuhenkilo_comboBox";
+            this.vastuuhenkilo_comboBox.Size = new System.Drawing.Size(176, 33);
+            this.vastuuhenkilo_comboBox.TabIndex = 14;
+            this.vastuuhenkilo_comboBox.SelectedIndexChanged += new System.EventHandler(this.vastuuhenkilo_comboBox_SelectedIndexChanged);
+            this.vastuuhenkilo_comboBox.TextChanged += new System.EventHandler(this.vastuuhenkilo_comboBox_TextChanged);
+            // 
+            // henkilot_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 322);
+            this.Controls.Add(this.vastuuhenkilo_comboBox);
+            this.Controls.Add(this.oppilaitos_comboBox);
+            this.Controls.Add(this.puheli_henkilo_label);
+            this.Controls.Add(this.sahkopost_label);
+            this.Controls.Add(this.osasto_label);
+            this.Controls.Add(this.titteli_label);
+            this.Controls.Add(this.puhelin_label);
+            this.Controls.Add(this.postitoimi_label);
+            this.Controls.Add(this.postinuero_label);
+            this.Controls.Add(this.osoite_label);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "henkilot_Form";
+            this.Text = "Henkilöt";
+            this.Load += new System.EventHandler(this.henkilot_Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label osoite_label;
+        private System.Windows.Forms.Label postinuero_label;
+        private System.Windows.Forms.Label postitoimi_label;
+        private System.Windows.Forms.Label puhelin_label;
+        private System.Windows.Forms.Label puheli_henkilo_label;
+        private System.Windows.Forms.Label sahkopost_label;
+        private System.Windows.Forms.Label osasto_label;
+        private System.Windows.Forms.Label titteli_label;
+        private System.Windows.Forms.ComboBox oppilaitos_comboBox;
+        private System.Windows.Forms.ComboBox vastuuhenkilo_comboBox;
+    }
+}
+
---
+++ tehtava18/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace tehtava18
+{
+    public partial class henkilot_Form : Form
+    {
+        DataTable oppilaitos = new DataTable();
+        DataTable vastuuHenkilot = new DataTable();
+        DataTable yhteys = new DataTable();
+
+        public henkilot_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void henkilot_Form_Load(object sender, EventArgs e)
+        {
+            taytaOppilaitosTaulukko();
+            taytaVastuuHenkiloTaulukko();
+            oppilaitos_comboBox.DataSource = oppilaitos;
+            oppilaitos_comboBox.DisplayMember = "ONimi";
+        }
+
+        private void oppilaitos_comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string viite = oppilaitos.Rows[oppilaitos_comboBox.SelectedIndex]["OID"].ToString();
+            osoite_label.Text = oppilaitos.Rows[oppilaitos_comboBox.SelectedIndex]["OKatuosoite"].ToString();
+            postinuero_label.Text = oppilaitos.Rows[oppilaitos_comboBox.SelectedIndex]["OPostinumero"].ToString();
+            postitoimi_label.Text = oppilaitos.Rows[oppilaitos_comboBox.SelectedIndex]["OPostitoimipaikka"].ToString();
+            puhelin_label.Text = oppilaitos.Rows[oppilaitos_comboBox.SelectedIndex]["OPuhelin"].ToString();
+
+            yhteys = vastuuHenkilot.Select("OID =" + viite).CopyToDataTable();
+            vastuuhenkilo_comboBox.DataSource = yhteys;
+            vastuuhenkilo_comboBox.DisplayMember = "VNimi";
+        }
+
+        private void vastuuhenkilo_comboBox_TextChanged(object sender, EventArgs e)
+        {
+            titteli_label.Text = yhteys.Rows[vastuuhenkilo_comboBox.SelectedIndex]["VTitteli"].ToString();
+            osasto_label.Text = yhteys.Rows[vastuuhenkilo_comboBox.SelectedIndex]["VSijainti"].ToString();
+            sahkopost_label.Text = yhteys.Rows[vastuuhenkilo_comboBox.SelectedIndex]["VSahkoposti"].ToString();
+            puhelin_label.Text = yhteys.Rows[vastuuhenkilo_comboBox.SelectedIndex]["VPuhelin"].ToString();
+        }
+
+        private void taytaOppilaitosTaulukko()
+        {
+            oppilaitos.Columns.Add("OID", typeof(int));
+            oppilaitos.Columns.Add("ONimi");
+            oppilaitos.Columns.Add("OKatuosoite");
+            oppilaitos.Columns.Add("OPostinumero");
+            oppilaitos.Columns.Add("OPostitoimipaikka");
+            oppilaitos.Columns.Add("OPuhelin");
+            // Alueet
+            oppilaitos.Rows.Add(1, "StadinAO", "Haatulantie 2", "00550", "Helsinki", "09 319 8600");
+            oppilaitos.Rows.Add(2, "Omnia", "Armas Launiksen katu 9", "02650", "Espoo", "046 887 1371");
+            oppilaitos.Rows.Add(3, "varia", "Rälssitie 13", "01530", "vantaa", "040 182 4668");
+            oppilaitos.Rows.Add(4, "Keuda", "Sibeliuksenväylä 55 A", "04400", "Järvenpää", "09 27 381");
+        }
+
+        private void taytaVastuuHenkiloTaulukko()
+        {
+            vastuuHenkilot.Columns.Add("OID", typeof(int));
+            vastuuHenkilot.Columns.Add("VNimi");
+            vastuuHenkilot.Columns.Add("VTitteli");
+            vastuuHenkilot.Columns.Add("VSijainti");
+            vastuuHenkilot.Columns.Add("VSahkoposti");
+            vastuuHenkilot.Columns.Add("VPuhelin");
+            //Henkilöt
+            vastuuHenkilot.Rows.Add(1, "Sirpa Lindroos", "Rehtori", "Kampus 1", "sirpa.lindroos@hel.fi", "050 540 4434");
+            vastuuHenkilot.Rows.Add(1, "Hanna Laurila", "Rehtori", "Kaupus 2", "hanna.laurila@hel.fi", "040 676 5583");
+            vastuuHenkilot.Rows.Add(1, "Annele Ranta", "Rehtori", "Kampus 3", "annele.ranta@hel.fi", "040 631 55667");
+            vastuuHenkilot.Rows.Add(1, "Eeva Sahlman", "Rehtori", "Kampus 4", "eeva.sahlam@hel.fi", "040 336 1017");
+            vastuuHenkilot.Rows.Add(1, "Marko Aaltonen", "Rehtori", "Kampus 5", "eeva.sahlman@hel.fi", "040 336 1017");
+            vastuuHenkilot.Rows.Add(2, "Tuula Antola", "Koulutuskuntayhtymän johtaja", "Yleishalinto", "tuula.antola 2omnia.fi", "");
+            vastuuHenkilot.Rows.Add(2, "Tapio Siukonen", "Hallintojohtaja", "Yleishallinto", "tapio.siukonen@omia.fi", "044 369 6600");
+            vastuuHenkilot.Rows.Add(2, "Tuukko Soini", "Kehittämisjohtaja", "Yleishallinto", "tuukka.soini@omnia.fi", "046 877 2952");
+            vastuuHenkilot.Rows.Add(2, "Riika-Maria Yli-Suomu", "Liiketoimintajohtaja", "Elinvoima- ja työllisyyspalvelut", "riikka-maria.yli-suomu@omnia.fi", "050 348 6544");
+            vastuuHenkilot.Rows.Add(2, "Maija Aaltola", "Rehtori", "Koulutus- ja opiskelijapalvelut", "maija-aaltola@omnia.fi", "050 384 9354");
+            vastuuHenkilot.Rows.Add(2, "Kai Iivari", "Talousjohtaja", "Talous ja hankintapalvelut", "maija-aalola@omnia.fi", "050 384 9354");
+            vastuuHenkilot.Rows.Add(2, "Päivi Korhonen", "Viestintäjohtaja", "Viestintä- ja markkinointipalvelut", "paivi.korhonen@omnia.fi", "040 126 7599");
+            vastuuHenkilot.Rows.Add(3, "Pekka Tauriainen", "Rehtori", "", "pekka.tauriainen@vantaa.fi", "050 312 4514");
+            vastuuHenkilot.Rows.Add(3, "Anne Heinonen", "Työelämäpalvelupäällikkö", "", "anne.heinonnen@vantaa.fi", "040 524 9048");
+            vastuuHenkilot.Rows.Add(3, "Tuula Heinonen", "Yhteisten palveluiden päällikkö", "", "anne.heinonen@vantaa.fi", "040 524 1242");
+            vastuuHenkilot.Rows.Add(4, "Tiina Halmevuo", "Kuntayhtymän johtaja", "Hallinto- ja johtamispalvelut", "tiina.halmevuo@keuda.fi", "050 336 9709");
+            vastuuHenkilot.Rows.Add(4, "Anna Mari Leinonen", "Rehtori", "Hallinto", "annamari.leinonen@keuda.fi", "040 174 4523");
+            vastuuHenkilot.Rows.Add(4, "Anne Vuorinen", "Johtaja", "Yritys_ ja elinvoimapalvelut", "anne.vuorinen@keuda.fi", "050 174 4523");
+            vastuuHenkilot.Rows.Add(4, "Hanna Nyrönen", "Vestintä- ja markkinointipäällikkö", "anne.vuorinen@keuda.fi", "050 415 0974");
+            vastuuHenkilot.Rows.Add(4, "Maarit Flinck", "Asianhallintapäällikkö", "Hallinto- ja johtamispalvelut", "maarit.flinck@keuda.fi", "0500 837 357");
+
+        }
+
+        private void vastuuhenkilo_comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
